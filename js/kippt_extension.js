ba69bfac26aa56386fbd6f37b5c4587fe9928748
Joao Carlos <mail@joao-carlos.com>
2012-08-01 23:54:58 +0300

    Cleanup and fixes

--- js/kippt_extension.js
+++ js/kippt_extension.js
@@ -1,93 +1,98 @@
 jQuery(function() {
   var Kippt = {
-    userId: null
+    popover: {},
+    clip: {},
+    existingClip: {},
+    user: {}
   };
   
   // Load user data from cache
-  if (localStorage.getItem('kipptUserId')) {
-    Kippt.userId = localStorage.getItem('kipptUserId');
+  if (localStorage.getItem("kipptUserId")) {
+    Kippt.user.id = +localStorage.getItem("kipptUserId");
   }
 
-  Kippt.closePopover = function() {
+  Kippt.popover.close = function() {
     setTimeout(function() {
       safari.extension.popovers[0].hide();
     }, 0);
   };
 
-  Kippt.openTab = function(url) {
+  Kippt.popover.openTab = function(url) {
     safari.application.activeBrowserWindow.openTab().url = url;
-    Kippt.closePopover();
+    Kippt.popover.close();
   };
 
-  Kippt.updateLists = function(data) {
-    $('#id_list').html('');
+  Kippt.popover.updateLists = function(data) {
+    $("#id_list").html("");
 
     $.each(data, function(i, list) {
-      var title;
+      var title,
+          selectedListElement;
       
       // Add user to title if not the current user
-      if (Kippt.userId && Kippt.userId !== list.user.id) {
-        title = list.title + ' (' + list.user.username + ')';
+      if (Kippt.user.id && Kippt.user.id !== list.user.id) {
+        title = list.title + " (" + list.user.username + ")";
       }
       else {
         title = list.title;
       }
       
-      $('#id_list').append(new Option(title, list.id, true, true));
+      $("#id_list").append(new Option(title, list.id, true, true));
     });
 
-    $('#id_list option').first().attr('selected', 'selected');
+    if (Kippt.clip.selectedList) {
+      selectedListElement = $("#id_list option[value='"+Kippt.clip.selectedList+"']");
+    }
+    else {
+      selectedListElement = $("#id_list option").first();
+    }
 
-    $('#id_list').append('<option id="new-list-toggle">-- New list --</option>');
-    $('#id_list').on('change', function(){
-      if ($(this).children("option#new-list-toggle:selected").length) {
-        $('#id_list').hide();
-        $('#new_list').css('display', 'inline-block');
-        $('#id_new_list').focus();
-      }
-    });
+    $("#id_list option").removeAttr("selected");
+    selectedListElement.attr("selected", "selected");
+
+    $("#id_list").append("<option id='new-list-toggle'>-- New list --</option>");
   };
   
-  Kippt.clearUI = function() {
+  Kippt.popover.clearUI = function() {
     $("#id_title").val("");
     $("#id_notes").val("");
     $("#id_is_read_later").removeAttr("checked");
     $("#id_list").show();
     $("#new_list").hide();
-    $('#id_new_list').val("");
-    $('#id_private').removeAttr("checked");
+    $("#id_new_list").val("");
+    $("#id_private").removeAttr("checked");
   };
   
-  Kippt.postClip = function(msg) {
+  Kippt.popover.saveClip = function(msg) {
     var type;
     if (!msg.id) {
       // Create new
-      type = 'POST';
-      url = 'https://kippt.com/api/clips/';
+      type = "POST";
+      url = "https://kippt.com/api/clips/";
     } else {
       // Update
-      type = 'PUT';
-      url = 'https://kippt.com/api/clips/'+msg.id+'/';
+      type = "PUT";
+      url = "https://kippt.com/api/clips/"+msg.id+"/";
     }
 
     var request = $.ajax({
       url: url,
       type: type,
-      dataType: 'json',
+      dataType: "json",
       data: JSON.stringify(msg)
     })
     .done(function(){
       // Clear page cache
-      Kippt.clearUI();
-      localStorage.removeItem('cache-title');
-      localStorage.removeItem('cache-notes');
+      Kippt.popover.clearUI();
+      localStorage.removeItem("cache-title");
+      localStorage.removeItem("cache-notes");
     })
     .fail(function(jqXHR, textStatus){
       alert( "Something went wrong when saving. Try again or contact hello@kippt.com");
     });
   };
 
-  Kippt.spinnerOptions = {
+  Kippt.popover.spinnerOptions = {
     lines: 9,
     length: 2,
     width: 2,
@@ -104,49 +109,47 @@ jQuery(function() {
     left: "auto"
   };
 
-  Kippt.spinner = new Spinner(Kippt.spinnerOptions).spin();
+  Kippt.popover.spinner = new Spinner(Kippt.popover.spinnerOptions).spin();
 
-  Kippt.popoverHandler = function() {
-    Kippt.existingClip = {};
-    Kippt.updateExisting = false;
-
-    $(".existing a").hide();
-    $(".existing").show();
-    $(".existing .loading").html($(Kippt.spinner.el).clone());
-
+  Kippt.popover.handler = function() {
     var tab = safari.application.activeBrowserWindow.activeTab;
     Kippt.url = tab.url;
 
-    // Open Kippt's Inbox if it's an empty page
+    // Open Kippt"s Inbox if it"s an empty page
     if (!tab.url) {
       tab.url = "https://kippt.com/inbox";
-      Kippt.closePopover();
+      Kippt.popover.close();
       return;
     }
 
     // Clear fields if not on the same page anymore
     if (tab.url !== Kippt.previousUrl) {
       $("#id_title").val(tab.title.trim());
-      Kippt.updateExisting = false;
+      Kippt.clip.updateExisting = false;
       Kippt.previousUrl = tab.url;
+      Kippt.existingClip = {};
+      delete Kippt.clip.selectedList;
+      $(".existing a").hide();
+      $(".existing").show();
+      $(".existing .loading").html($(Kippt.popover.spinner.el).clone());
     }
 
     // Authentication check and connected services
     $.getJSON("https://kippt.com/api/account/?include_data=services&disable_basic_auth=1")
     .done(function(data) {
       Kippt.profilePath = data.app_url;
-      Kippt.userId = data.id;
-      localStorage.setItem('kipptUserId', data.id);
+      Kippt.user.id = data.id;
+      localStorage.setItem("kipptUserId", data.id);
 
       $.each(data.services, function(name, connected) {
         if (connected) {
           $("#kippt-actions ." + name).toggleClass("connected", connected);
-          $("#kippt-actions ." + name).css('display', 'inline-block');
+          $("#kippt-actions ." + name).css("display", "inline-block");
         }
       });
     })
     .fail(function() {
-      Kippt.openTab("https://kippt.com/login/");
+      Kippt.popover.openTab("https://kippt.com/login/");
     });
 
     // Check for existing clip
@@ -162,7 +165,7 @@ jQuery(function() {
     // Populate lists from local storage
     var listCache = localStorage.getItem("kipptListCache");
     if (listCache) {
-      Kippt.updateLists(JSON.parse(localStorage.getItem("kipptListCache")));
+      Kippt.popover.updateLists(JSON.parse(localStorage.getItem("kipptListCache")));
     }
 
     // Fetch latest lists
@@ -172,7 +175,7 @@ jQuery(function() {
           listJSON = JSON.stringify(lists);
 
       if (listJSON !== listCache) {
-        Kippt.updateLists(lists);
+        Kippt.popover.updateLists(lists);
         localStorage.setItem("kipptListCache", listJSON);
       }
     });
@@ -181,8 +184,9 @@ jQuery(function() {
   // Edit existing clip
   $(document).on("click", ".existing a", function(event) {
     event.preventDefault();
-    Kippt.clearUI();
-    Kippt.updateExisting = true;
+    Kippt.popover.clearUI();
+    Kippt.clip.updateExisting = true;
+    Kippt.clip.selectedList = Kippt.existingClip.list.id;
     $("#id_title").val(Kippt.existingClip.title);
     $("#id_notes").val(Kippt.existingClip.notes);
     $("#id_list option[value="+Kippt.existingClip.list.id+"]").attr("selected", "selected");
@@ -204,62 +208,72 @@ jQuery(function() {
       share: services
     };
     
-    if (Kippt.updateExisting) {
+    if (Kippt.clip.updateExisting) {
       data.id = Kippt.existingClip.id;
     }
 
     // New list
-    if ($('#id_new_list').val()) {
+    if ($("#id_new_list").val()) {
       data.new_list = {};
-      data.new_list.title = $('#id_new_list').val();
-      if ($('#id_private').is(':checked')) {
+      data.new_list.title = $("#id_new_list").val();
+      if ($("#id_private").is(":checked")) {
         data.new_list.is_private = true;
       }
       else {
         data.new_list.is_private = false;
       }
     }
-
     
     if (data.new_list) {
       $.ajax({
-        url: 'https://kippt.com/api/lists/',
-        type: 'POST',
-        dataType: 'json',
+        url: "https://kippt.com/api/lists/",
+        type: "POST",
+        dataType: "json",
         data: JSON.stringify(data.new_list)
       })
-      .done(function(data){
+      .done(function(response) {
         // Create clip with new list
-        data.list = data.id;
-        Kippt.postClip(data);
+        data.list = response.id;
+        Kippt.popover.saveClip(data);
       })
-      .fail(function(){
+      .fail(function() {
         alert( "Something went wrong when saving. Try again or contact hello@kippt.com");
       });
     } else {
       // Create clip with existing list
-      Kippt.postClip(data);
+      Kippt.popover.saveClip(data);
     }
     
-    Kippt.closePopover();
+    Kippt.popover.close();
   });
 
   // Connect a service to share
   $(document).on("click", "#kippt-actions > div:not(.connected)", function() {
-    Kippt.openTab("https://kippt.com/accounts/settings/connections/");
-    Kippt.closePopover();
+    Kippt.popover.openTab("https://kippt.com/accounts/settings/connections/");
+    Kippt.popover.close();
   });
 
   // Open profile page
   $(document).on("click", "#open-profile", function() {
     if (Kippt.profilePath !== undefined) {
-      Kippt.openTab("https://kippt.com" + Kippt.profilePath);
+      Kippt.popover.openTab("https://kippt.com" + Kippt.profilePath);
     }
   });
 
   // Open inbox page
   $(document).on("click", "#open-inbox", function() {
-    Kippt.openTab("https://kippt.com/inbox");
+    Kippt.popover.openTab("https://kippt.com/inbox");
+  });
+
+  $("#id_list").on("change", function(event) {
+    if ($(this).children("option#new-list-toggle:selected").length) {
+      $("#id_list").hide();
+      $("#new_list").css("display", "inline-block");
+      $("#id_new_list").focus();
+    }
+    else {
+      Kippt.clip.selectedList = $("#id_list option:selected").val();
+    }
   });
 
   // Configure share tooltips
@@ -276,5 +290,5 @@ jQuery(function() {
     }
   });
 
-  safari.application.addEventListener("popover", Kippt.popoverHandler, true);
+  safari.application.addEventListener("popover", Kippt.popover.handler, true);
 });
